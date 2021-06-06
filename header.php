a7ea082f3cda1e338d9bf34be2f41dd76e208377
MacPika <enkhbaatar.azaa@icloud.com>
2021-06-06 22:49:03 +0800

    les9: Безопасный вывод данных (Escaping)

--- header.php
+++ header.php
@@ -12,21 +12,38 @@
 <?php wp_body_open(); ?>
 
 <?php
-  wp_nav_menu(
+    wp_nav_menu(
       array(
           'theme_location' => 'header_nav',
           'menu_class' => 'myclass'
       )
-  );
+    );
 
-  get_search_form();
+    get_search_form();
 
-  $name = 'Alex Sochirka aka "CRIKOVA"';
-  $name = 'Alex Sochirka aka strongCRIKOVA"';
+    $name = 'Alex Sochirka aka "CRIKOVA"';
+    $name2 = 'Alex Sochirka aka <strong>CRIKOVA</strong>"';
 
-
+    //esc_attr() - attributes escape
+    //esc_html() - html escape
+    //esc_url() - link escape
+    //wp_kses() -
+    $name3 = 'Alex <a hreff="#">Sochirka</a> aka <strong>CRIKOVA</strong>';
 
 ?>
 
     <input name=""author" value="<?php echo esc_attr($name); ?>" />
     <!-- esc_attr обязателе в html атрибьютах иначе будуд ошибки, к примеру текст в двойных ковычках не выводится -->
+    <?php echo esc_html($name2); ?>
+    <!-- esc_html, html не интерпретируются, как теги -->
+    <a href="<?php echo esc_url(home_url("/")) ?>">Home</a>
+<?php
+    $args = array(
+            'a' => array(
+                    'href' => array()
+            ),
+            'strong' => array(
+            )
+    );
+    echo wp_kses($name3, $args);
+?>
