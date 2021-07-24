796dba400339c8e3fdac6ad0688bc109784169b9
MacPika <31432411+MacPika@users.noreply.github.com>
2021-07-24 16:45:52 +0800

    demo code

--- header.php
+++ header.php
@@ -67,3 +67,94 @@
     <div class="apf_main_container cf"><!-- Main Container Start -->
         <div class="apf_container">
 
+            class ale_demo_category extends ale_demo_base {
+
+            static function add_term( $params_array ) {
+
+            self::check_params( __CLASS__, __FUNCTION__, $params_array, array(
+            'term_name' => 'Param is required!',
+            'taxonomy'  => 'Param is required',
+            ) );
+
+            wp_insert_term( $params_array['term_name'], // the term
+            $params_array['taxonomy'], // the taxonomy
+            array(
+            'description' => $params_array['description'],
+            'parent'      => $params_array['parent_id']
+            ) );
+
+            $parent_term = term_exists( $params_array['term_name'], $params_array['taxonomy'] ); // array is returned if taxonomy is given
+            $new_term_id = $parent_term['term_id']; // get numeric term id
+
+            if ( ! empty( $params_array['ale_type_icon'] ) ) {
+            $type_icon_url = ale_demo_media::get_image_url_by_ale_id( $params_array['ale_type_icon'] );
+            $type_icon_id  = ale_demo_media::get_by_ale_id( $params_array['ale_type_icon'] );
+
+            $type_icon = array( 'id' => $type_icon_id, 'url' => $type_icon_url );
+            Tax_Meta_Class::update_tax_meta( $new_term_id, 'ale_image_field_id', $type_icon );
+            }
+
+            // keep a list of installed category ids so we can delete them later if needed
+            $ale_stacks_demo_terms_id   = get_option( 'ale_demo_terms_id' );
+            if(!is_array($ale_stacks_demo_terms_id)) {
+            $ale_stacks_demo_terms_id = array();
+            }
+            $ale_stacks_demo_terms_id[] = array( 'id' => $new_term_id, 'taxonomy' => $params_array['taxonomy'] );
+
+            update_option( 'ale_demo_terms_id', $ale_stacks_demo_terms_id );
+
+            return $new_term_id;
+            }
+
+            static function remove_term() {
+            $ale_stacks_demo_terms_id = get_option( 'ale_demo_terms_id' );
+            if ( is_array( $ale_stacks_demo_terms_id ) ) {
+            foreach ( $ale_stacks_demo_terms_id as $ale_stacks_demo_term ) {
+            wp_delete_term( $ale_stacks_demo_term['id'], $ale_stacks_demo_term['taxonomy'] );
+            }
+            }
+            //Clear removed terms id
+            update_option( 'ale_demo_terms_id', '' );
+            }
+
+            static function add_category( $params_array ) {
+
+            self::check_params( __CLASS__, __FUNCTION__, $params_array, array(
+            'category_name' => 'Param is required!',
+            ) );
+
+            if ( empty( $params_array['parent_id'] ) ) {
+            $new_cat_id = wp_create_category( $params_array['category_name'] );
+            } else {
+            $new_cat_id = wp_create_category( $params_array['category_name'], $params_array['parent_id'] );
+            }
+
+            //update category descriptions
+            if ( ! empty( $params_array['description'] ) ) {
+            wp_update_term( $new_cat_id, 'category', array(
+            'description' => $params_array['description']
+            ) );
+            }
+
+            // keep a list of installed category ids so we can delete them later if needed
+            $ale_stacks_demo_categories_id   = get_option( 'ale_demo_categories_id' );
+            if(!is_array($ale_stacks_demo_categories_id)) {
+            $ale_stacks_demo_categories_id = array();
+            }
+            $ale_stacks_demo_categories_id[] = $new_cat_id;
+            update_option( 'ale_demo_categories_id', $ale_stacks_demo_categories_id );
+
+            return $new_cat_id;
+            }
+
+            static function remove() {
+            $ale_stacks_demo_categories_id = get_option( 'ale_demo_categories_id' );
+            if ( is_array( $ale_stacks_demo_categories_id ) ) {
+            foreach ( $ale_stacks_demo_categories_id as $ale_stacks_demo_category_id ) {
+            wp_delete_category( $ale_stacks_demo_category_id );
+            }
+            }
+            //Clear removed categories id
+            update_option( 'ale_demo_categories_id', '' );
+            }
+            }
